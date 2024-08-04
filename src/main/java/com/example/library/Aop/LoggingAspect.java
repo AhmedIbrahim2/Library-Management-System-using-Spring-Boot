429c50b6e2a1877c105ceb6e4103dc78249d4891
Ahmed Ibrahim <ahmed.aboali9874@gmail.com>
2024-08-04 17:43:13 +0300

    update Aop

--- src/main/java/com/example/library/Aop/LoggingAspect.java
+++ src/main/java/com/example/library/Aop/LoggingAspect.java
@@ -2,21 +2,38 @@ package com.example.library.Aop;
 
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
 @Aspect
 @Component
 public class LoggingAspect {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @After("execution(* com.example.library.service.*.*(..))")
-    public void logBeforeAllMethods(JoinPoint joinPoint) {
-        logger.info("A method in the service package is called : " + joinPoint.getSignature().getName());
+    @Before("execution(* com.example.library.service.BookService.addBook(..)) || execution(* com.example.library.service.BookService.updateBook(..)) || execution(* com.example.library.service.PatronService.addPatron(..)) || execution(* com.example.library.service.PatronService.updatePatron(..))")
+    public void logBeforeSpecificMethods(JoinPoint joinPoint) {
+        logger.info("Entering method: " + joinPoint.getSignature().getName());
+    }
+
+    @After("execution(* com.example.library.service.BookService.addBook(..)) || execution(* com.example.library.service.BookService.updateBook(..)) || execution(* com.example.library.service.PatronService.addPatron(..)) || execution(* com.example.library.service.PatronService.updatePatron(..))")
+    public void logAfterSpecificMethods(JoinPoint joinPoint) {
+        logger.info("Exiting method: " + joinPoint.getSignature().getName());
+    }
+
+    @AfterThrowing(pointcut = "execution(* com.example.library.service.BookService.addBook(..)) || execution(* com.example.library.service.BookService.updateBook(..)) || execution(* com.example.library.service.PatronService.addPatron(..)) || execution(* com.example.library.service.PatronService.updatePatron(..))", throwing = "error")
+    public void logExceptionsSpecificMethods(JoinPoint joinPoint, Throwable error) {
+        logger.error("Exception in method: " + joinPoint.getSignature().getName() + " with cause: " + (error.getCause() != null ? error.getCause() : "NULL"));
+    }
+
+    @Around("execution(* com.example.library.service.BookService.addBook(..)) || execution(* com.example.library.service.BookService.updateBook(..)) || execution(* com.example.library.service.PatronService.addPatron(..))|| execution(* com.example.library.service.PatronService.updatePatron(..))")
+    public Object logPerformanceSpecificMethods(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object returnValue = joinPoint.proceed();
+        long timeTaken = System.currentTimeMillis() - start;
+        logger.info("Method " + joinPoint.getSignature().getName() + " executed in " + timeTaken + "ms");
+        return returnValue;
     }
 }
